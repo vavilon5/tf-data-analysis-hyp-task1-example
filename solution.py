f07a7428759c452aa33fd05e7b21d0507a12fd8a
vavilon5 <107186119+vavilon5@users.noreply.github.com>
2023-04-10 22:48:25 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 729362937 # Ваш chat ID, не меняйте название переменной
 
 def solution(x_success: int, 
              x_cnt: int, 
@@ -11,4 +11,5 @@ def solution(x_success: int,
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    
+    return (abs(y_success/y_cnt - x_success/x_cnt) > 0.05) # Ваш ответ, True или False
