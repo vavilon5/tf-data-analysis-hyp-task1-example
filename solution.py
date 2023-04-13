adea795fb791f708384969ef0fa72f37074f1e00
vavilon5 <107186119+vavilon5@users.noreply.github.com>
2023-04-13 22:03:11 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -8,8 +8,16 @@ def solution(x_success: int,
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
     
-    return (y_success/y_cnt - x_success/x_cnt > 0.05) # Ваш ответ, True или False
+    from statsmodels.stats.proportion import proportions_ztest
+    
+    count = np.array([x_success, y_success])
+    nobs = np.array([x_cnt, y_cnt])
+    value = 0.05
+    alternative = 'smaller'
+    
+    stat, pval = proportions_ztest(count, nobs, value=value, alternative=alternative)
+    
+    
+    return (pval < 0.05) 
+    
